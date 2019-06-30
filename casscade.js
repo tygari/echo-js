982e76ffae18a5fb5ee91d93cb61139235a22f6f
Tygari <tygarimew@yahoo.com>
2019-06-30 16:36:52 -0400

    Empty Echo bug fixed, Code Refined
    If an Empty Echo attribute was set it would cause a bug of a single ID'less child being created.
    This was fixed by adding a check for this.
    
    The code was refactored to have a smaller footprint by removing a variable by reusing an established variable.
    And by moving all the variables to be defined by a single 'let' instead of multiple 'let''s.
    Finally, the word function was replaced with an arrow function shorthand.

--- casscade.js
+++ casscade.js
@@ -1,20 +1,21 @@
 (()=>{customElements.define('cass-cade',class extends HTMLElement{
 	static get observedAttributes(){return ['echo','code']};
 	attributeChangedCallback(name,oldValue){
-		let ID =x=>(document.getElementById(x));
-		let code = this.getAttribute('code');
+		let ID=x=>(document.getElementById(x)),
+			gA=x=>(this.getAttribute(x)),
+			code=gA('code'),
+			v,i,o={};
 		if(!code){code='<div></div>'};
-		let o,n,i,obj={};
 		if(name=='echo'&&oldValue){
-			o = oldValue.split(" ");
-			for(i=0;i<o.length;i++){if(ID(o[i])){obj[o[i]]=ID(o[i]).outerHTML}};
+			v=oldValue.split(" ");
+			for(i=0;i<v.length;i++){if(ID(v[i])){o[v[i]]=ID(v[i]).outerHTML}};
 		}
-		if(this.hasAttribute('echo')&&this.getAttribute('echo')!==""){
-			n = this.getAttribute('echo').split(" ");
+		if(this.hasAttribute('echo')&&gA('echo')!==""){
+			v=gA('echo').split(" ");
 			this.innerHTML = '';
-			for(i=0;i<n.length;i++){
-				this.insertAdjacentHTML('beforeend',(obj[n[i]]?obj[n[i]]:code));
-				this.lastElementChild.id = n[i];
+			for(i=0;i<v.length;i++){
+				this.insertAdjacentHTML('beforeend',(o[v[i]]?o[v[i]]:code));
+				this.lastElementChild.id = v[i];
 			}
 		}
 	}
