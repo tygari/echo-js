a32ab924cfeabeca40330339508592e52dd16006
Tygari <tygarimew@yahoo.com>
2019-07-02 18:58:15 -0400

    Tag name changed, Bug fixes
    Changed the tag name from <cass-cade> to <echo-> to be more clear.
    The last update prevented users from clearing children by setting echo to an empty string.
    This has been fixed.
    The code was improved to prevent most whitespace children.
    
    Two words separated by a Tab are glitching by combining the words with the tab as one for the ID of the child.

---
+++ echo.js
@@ -0,0 +1,24 @@
+(()=>{customElements.define('echo-',class extends HTMLElement{
+	static get observedAttributes(){return ['echo','code']};
+	attributeChangedCallback(name,oldValue){
+		let I=x=>(document.getElementById(x)),
+			G=x=>(this.getAttribute(x)),
+			R=x=>(x.trim().replace(/\s\s+/g,' ').split(" ")),
+			c=G('code'),v,i,o={};
+		if(!c){c='<div></div>'};
+		if(name=='echo'&&oldValue){
+			v=R(oldValue);
+			for(i=0;i<v.length;i++){if(I(v[i])){o[v[i]]=I(v[i]).outerHTML}};
+		}
+		if(this.hasAttribute('echo')){
+			v=R(G('echo'));
+			this.innerHTML = '';
+			if(v[0]!==""){
+				for(i=0;i<v.length;i++){
+					this.insertAdjacentHTML('beforeend',(o[v[i]]?o[v[i]]:c));
+					this.lastElementChild.id = v[i];
+				}
+			}
+		}
+	}
+})})();
