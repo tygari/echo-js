d6212fc13c0edf3e98b8bad0f948748402fd4151
Tygari <26011255+tygari@users.noreply.github.com>
2019-10-28 18:12:07 -0400

    Added assist error correction
    the console will now display some issues when there is an error.

--- echo.js
+++ echo.js
@@ -5,7 +5,13 @@
 			G=x=>(this.getAttribute(x)),
 			R=x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v/g,' ').split(' ')),
 			c=G('code'),v,i,o={};
-		if(!c||!c.startsWith('<')||!c.endsWith('>')){c='<div></div>'};
+		if(!c||!c.startsWith('<')||!c.endsWith('>')){
+			console.log(this.outerHTML);
+			let cr='color:red',cb='color:black',co='color:orange';
+			c?console.log('%cERROR: %c'+c+'%c is not a proper HTML code string.  %cDefaulting to: %c<div></div>%c.',cr,cb,cr,co,cb,co)
+			:console.log('%cERROR: TAG <echo-> has no code attribute.  '+'%cDefaulting to: %c<div></div>%c.',cr,co,cb,co);
+			c='<div></div>';
+		}
 		if(name=='echo'&&oldValue){
 			v=R(oldValue);
 			for(i=0;i<v.length;i++){if(I(v[i])){o[v[i]]=I(v[i]).outerHTML}};
