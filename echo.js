32372b3c9d4dc482e7b5e28cf023901a0ca66d10
Tygari <26011255+tygari@users.noreply.github.com>
2020-03-04 15:04:12 -0500

    Template Tag Functionality Added
    Added functionality for template tag HTML retrieval.
    Rewrote the variables to be more readable to other people.

--- echo.js
+++ echo.js
@@ -1,28 +1,41 @@
-(()=>{customElements.define('echo-',class extends HTMLElement{
-	static get observedAttributes(){return['echo','code']};
+(()=>{customElements.define(`echo-`,class extends HTMLElement{
+	static get observedAttributes(){return[`echo`,`code`]};
 	attributeChangedCallback(name,oldValue){
-		let I=x=>(document.getElementById(x)),
-			G=x=>(this.getAttribute(x)),
-			R=x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v/g,' ').split(' ')),
-			c=G('code'),v,i,o={};
-		if(!c||!c.startsWith('<')||!c.endsWith('>')){
+		let ID =x=>(document.getElementById(x)),
+			ATTR =x=>(this.getAttribute(x)),
+			TRIM =x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v/g,` `)),
+			EVAL =x=>{
+				try {if (!!eval(ATTR(`code`))){return true}}
+				catch {return false}
+			},
+			CODE = EVAL() ? TRIM(eval(ATTR(`code`)).innerHTML) : ATTR(`code`),
+			VALUE,
+			INC,
+			OBJ = {};
+		if (!CODE || !CODE.startsWith(`<`) || !CODE.endsWith(`>`)){
 			console.log(this.outerHTML);
-			let cr='color:red',cb='color:black',co='color:orange';
-			c?console.log('%cERROR: %c'+c+'%c is not a proper HTML code string.  %cDefaulting to: %c<div></div>%c.',cr,cb,cr,co,cb,co)
-			:console.log('%cERROR: TAG <echo-> has no code attribute.  '+'%cDefaulting to: %c<div></div>%c.',cr,co,cb,co);
-			c='<div></div>';
+			let cr=`color:red`,
+				cb=`color:blue`,
+				co=`color:orange`;
+			CODE ? console.log(`%cERROR: ATTRIBUTE "code" %c${CODE} %cis not a proper HTML code string.  %cDefaulting to: %c<div></div>%c.`,cr,cb,cr,co,cb,co)
+				 : console.log(`%cERROR: TAG <echo-> is missing ATTRIBUTE "code". %cDefaulting to: %c<div></div>%c.`,cr,co,cb,co);
+			CODE =`<div></div>`;
 		}
-		if(name=='echo'&&oldValue){
-			v=R(oldValue);
-			for(i=0;i<v.length;i++){if(I(v[i])){o[v[i]]=I(v[i]).outerHTML}};
+		if (name == `echo` && oldValue){
+			VALUE = TRIM(oldValue).split(` `);
+			for(INC = 0; INC < VALUE.length; INC++){
+				if(ID(VALUE[INC])){
+					OBJ[VALUE[INC]]=ID(VALUE[INC]).outerHTML
+				}
+			};
 		}
-		if(this.hasAttribute('echo')){
-			v=R(G('echo'));
-			this.innerHTML='';
-			if(v[0]!==""){
-				for(i=0;i<v.length;i++){
-					this.insertAdjacentHTML('beforeend',(o[v[i]]?o[v[i]]:c));
-					this.lastElementChild.id=v[i];
+		if (this.hasAttribute(`echo`)){
+			VALUE = TRIM(ATTR(`echo`)).split(` `);
+			this.innerHTML = ``;
+			if(VALUE[0] !== ``){
+				for(INC = 0; INC < VALUE.length; INC++){
+					this.insertAdjacentHTML( `beforeend` , (OBJ[VALUE[INC]] ? OBJ[VALUE[INC]] : CODE));
+					this.lastElementChild.id = VALUE[INC];
 				}
 			}
 		}
