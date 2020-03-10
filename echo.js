b56bedabcc6fd8f8781c3df199f2bdd56b5cf006
Tygari <26011255+tygari@users.noreply.github.com>
2020-03-10 19:23:06 -0400

    UPDATE: Arrays and Global Variables (arrays, objects, and strings)
    The echo attribute is now compatible with Arrays and Global Variables containing arrays, objects, and strings.
    Objects will have the keys converted into the ids, not their key values.

--- echo.js
+++ echo.js
@@ -2,16 +2,37 @@ if (!!window.customElements){
 	(()=>{customElements.define(`echo-`,class extends HTMLElement{
 		static get observedAttributes(){return[`echo`,`code`]};
 		attributeChangedCallback(name,oldValue){
-			let ID =x=>(document.getElementById(x)),
+			let VALUE,
+				INC,
+				CHK,
+				ID =x=>(document.getElementById(x)),
 				ATTR =x=>(this.getAttribute(x)),
+				ARR =x=>(Array.isArray(x)),
 				TRIM =x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v/g,` `)),
 				EVAL =x=>{
-					try {if (!!eval(ATTR(`code`))){return true}}
+					try {if (!!eval(x)){return true}}
 					catch(e){return false}
 				},
-				CODE = EVAL() ? TRIM(eval(ATTR(`code`)).innerHTML) : ATTR(`code`),
-				VALUE,
-				INC,
+				ECHO =x=>{
+					if (!!window[TRIM(x)]){
+						CHK = window[TRIM(x)];
+						if (CHK !== null && CHK !== undefined){
+							if (typeof CHK == 'string'){x = TRIM(CHK).split(` `)}
+							else if (ARR(CHK)){x = CHK}
+							else if (typeof CHK == 'object' && !(CHK instanceof Element)){
+								VALUE = [];
+								for (INC in CHK){
+									VALUE.push(``+INC);
+								}
+								x = VALUE;
+							}
+						}
+					}
+					else if (EVAL(x) && ARR(eval(x))){x = eval(x)}
+					if (!ARR(x)){x = TRIM(x).split(` `)}
+					return x;
+				},
+				CODE = EVAL(ATTR(`code`)) ? TRIM(eval(ATTR(`code`)).innerHTML) : ATTR(`code`),
 				OBJ = {};
 			if (!CODE || !CODE.startsWith(`<`) || !CODE.endsWith(`>`)){
 				console.log(this.outerHTML);
@@ -21,25 +42,37 @@ if (!!window.customElements){
 				CODE ? console.log(`%cERROR: ATTRIBUTE "code" %c${CODE} %cis not a proper HTML code string.  %cDefaulting to: %c<div></div>%c.`,cr,cb,cr,co,cb,co)
 					 : console.log(`%cERROR: TAG <echo-> is missing ATTRIBUTE "code". %cDefaulting to: %c<div></div>%c.`,cr,co,cb,co);
 				CODE =`<div></div>`;
-			}
+			};
 			if (name == `echo` && oldValue){
-				VALUE = TRIM(oldValue).split(` `);
+				VALUE = ECHO(oldValue);
 				for(INC = 0; INC < VALUE.length; INC++){
 					if(ID(VALUE[INC])){
-						OBJ[VALUE[INC]]=ID(VALUE[INC]).outerHTML
+						OBJ[VALUE[INC]] = ID(VALUE[INC]).outerHTML
 					}
-				};
-			}
+				}
+			};
 			if (this.hasAttribute(`echo`)){
-				VALUE = TRIM(ATTR(`echo`)).split(` `);
+				VALUE = ECHO(ATTR(`echo`));
 				this.innerHTML = ``;
 				if(VALUE[0] !== ``){
 					for(INC = 0; INC < VALUE.length; INC++){
-						this.insertAdjacentHTML( `beforeend` , (OBJ[VALUE[INC]] ? OBJ[VALUE[INC]] : CODE));
-						this.lastElementChild.id = VALUE[INC];
+						if (typeof VALUE[INC] !== 'object' && typeof VALUE[INC] !== undefined){
+							this.insertAdjacentHTML( `beforeend` , (OBJ[VALUE[INC]] ? OBJ[VALUE[INC]] : CODE));
+							this.lastElementChild.id = ``+VALUE[INC];
+						} else {console.log(`%cERROR%c: Invalid ID Data Type.`,`color:red`,`color:black`)}
 					}
 				}
-			}
+			};
+			window.addEventListener(`load`,()=>{
+				if (!!window[TRIM(ATTR(`echo`))]){
+					CHK = window[TRIM(ATTR(`echo`))];
+					if (CHK !== null && CHK !== undefined){
+						if (typeof CHK == 'string' || ARR(CHK) || (typeof CHK == 'object' && !(CHK instanceof Element))){
+							this.setAttribute(`echo` , ATTR(`echo`));
+						}
+					}
+				}
+			});
 		}
 	})})()
 } else {console.log(`%cERROR%c: WEB COMPONENTS are Disabled`,`color:red`,`color:black`);}
