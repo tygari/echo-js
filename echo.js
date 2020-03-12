7877bf730bfd82e28fa3908d822c322d6f924fd4
Tygari <26011255+tygari@users.noreply.github.com>
2020-03-12 01:24:55 -0400

    Minor Optimizations
    Few tweaks

--- echo.js
+++ echo.js
@@ -1,21 +1,26 @@
 if (!!window.customElements){
 	(()=>{customElements.define(`echo-`,class extends HTMLElement{
-		static get observedAttributes(){return[`echo`,`code`]};
+		static get observedAttributes(){return[`echo`,`code`,`test`]};
 		attributeChangedCallback(name,oldValue){
-			let VALUE,
+			let CR=`color:red`,
+				CB=`color:blue`,
+				CO=`color:orange`,
+				VALUE,
 				INC,
 				CHK,
 				ID =x=>(document.getElementById(x)),
-				ATTR =x=>(this.getAttribute(x)),
+				GET =x=>(this.getAttribute(x)),
 				ARR =x=>(Array.isArray(x)),
-				TRIM =x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v/g,` `)),
+				TRIM =x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v|[,]/g,` `)),
+				WIN =x=>(window[TRIM(x)]),
 				EVAL =x=>{
 					try {if (!!eval(x)){return true}}
 					catch(e){return false}
 				},
+				CODE = EVAL(GET(`code`)) ? TRIM(eval(GET(`code`)).innerHTML) : GET(`code`),
 				ECHO =x=>{
-					if (!!window[TRIM(x)]){
-						CHK = window[TRIM(x)];
+					if (!!WIN(x)){
+						CHK = WIN(x);
 						if (CHK !== null && CHK !== undefined){
 							if (typeof CHK == 'string'){x = TRIM(CHK).split(` `)}
 							else if (ARR(CHK)){x = CHK}
@@ -32,48 +37,47 @@ if (!!window.customElements){
 					if (!ARR(x)){x = TRIM(x).split(` `)}
 					return x;
 				},
-				CODE = EVAL(ATTR(`code`)) ? TRIM(eval(ATTR(`code`)).innerHTML) : ATTR(`code`),
 				OBJ = {};
-			if (!CODE || !CODE.startsWith(`<`) || !CODE.endsWith(`>`)){
-				console.log(this.outerHTML);
-				let cr=`color:red`,
-					cb=`color:blue`,
-					co=`color:orange`;
-				CODE ? console.log(`%cERROR: ATTRIBUTE "code" %c${CODE} %cis not a proper HTML code string.  %cDefaulting to: %c<div></div>%c.`,cr,cb,cr,co,cb,co)
-					 : console.log(`%cERROR: TAG <echo-> is missing ATTRIBUTE "code". %cDefaulting to: %c<div></div>%c.`,cr,co,cb,co);
-				CODE =`<div></div>`;
-			};
-			if (name == `echo` && oldValue){
-				VALUE = ECHO(oldValue);
-				for(INC = 0; INC < VALUE.length; INC++){
-					if(ID(VALUE[INC])){
-						OBJ[VALUE[INC]] = ID(VALUE[INC]).outerHTML
-					}
+			if (GET(`echo`) != '[object Object]'){
+				if (!CODE || !CODE.startsWith(`<`) || !CODE.endsWith(`>`)){
+					console.log(this.outerHTML);
+					CODE ? console.log(`%cERROR: ATTRIBUTE "code" %c${CODE} %cis not a proper HTML code string.  %cDefaulting to: %c<div></div>%c.`,CR,CB,CR,CO,CB,CO)
+						 : console.log(`%cERROR: TAG <echo-> is missing ATTRIBUTE "code". %cDefaulting to: %c<div></div>%c.`,CR,CO,CB,CO);
+					CODE =`<div></div>`;
 				}
-			};
-			if (this.hasAttribute(`echo`)){
-				VALUE = ECHO(ATTR(`echo`));
-				this.innerHTML = ``;
-				if(VALUE[0] !== ``){
+				if (name == `echo` && oldValue){
+					VALUE = ECHO(oldValue);
 					for(INC = 0; INC < VALUE.length; INC++){
-						if (typeof VALUE[INC] !== 'object' && typeof VALUE[INC] !== undefined){
-							this.insertAdjacentHTML( `beforeend` , (OBJ[VALUE[INC]] ? OBJ[VALUE[INC]] : CODE));
-							this.lastElementChild.id = ``+VALUE[INC];
-						} else {console.log(`%cERROR%c: Invalid ID Data Type.`,`color:red`,`color:black`)}
-					}
-				}
-			};
-			window.addEventListener(`load`,()=>{
-				if (!!window[TRIM(ATTR(`echo`))]){
-					CHK = window[TRIM(ATTR(`echo`))];
-					if (CHK !== null && CHK !== undefined){
-						if (typeof CHK == 'string' || ARR(CHK) || (typeof CHK == 'object' && !(CHK instanceof Element))){
-							this.setAttribute(`echo` , ATTR(`echo`));
+						if(ID(VALUE[INC])){
+							OBJ[VALUE[INC]] = ID(VALUE[INC]).outerHTML
 						}
 					}
 				}
-			});
+				if (this.hasAttribute(`echo`)){
+					VALUE = ECHO(GET(`echo`));
+					this.innerHTML = ``;
+					if(VALUE[0] !== ``){
+						for(INC = 0; INC < VALUE.length; INC++){
+							if (typeof VALUE[INC] !== 'object' && typeof VALUE[INC] !== undefined){
+								this.insertAdjacentHTML( `beforeend` , (OBJ[VALUE[INC]] ? OBJ[VALUE[INC]] : CODE));
+								this.lastElementChild.id = ``+VALUE[INC];
+							} else {console.log(`%cERROR%c: Invalid ID Data Type.`,CR,CO)}
+						}
+					}
+				}
+				window.addEventListener(`load`,()=>{
+					if (!!WIN(GET(`echo`))){
+						CHK = WIN(GET(`echo`));
+						if (CHK !== null && CHK !== undefined){
+							if (typeof CHK == 'string' || ARR(CHK) || (typeof CHK == 'object' && !(CHK instanceof Element))){
+								this.setAttribute(`echo` , GET(`echo`));
+							}
+						}
+					}
+				})
+			} else {
+				console.log(`%cERROR%c: An object literal was passed as a string, resulting in echo="[object Object]".  HTML cannot process object literals.`,CR,CO);
+			}
 		}
 	})})()
-} else {console.log(`%cERROR%c: WEB COMPONENTS are Disabled`,`color:red`,`color:black`);}
 //Written by Tygari Katarana Davis
