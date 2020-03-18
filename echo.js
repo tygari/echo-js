2999d3cc2c13d08e65ce70615e9dccc28ca2b405
Tygari <26011255+tygari@users.noreply.github.com>
2020-03-18 17:49:43 -0400

    New Global Variable Dot Notation and Auto Attribute Features Added
    Global Variable Objects with Sub Key Dot Notation will now be recognized.
    New Attribute Auto.
    Attribute Auto is a boolean attribute of true or false.
    Anything not true is recognized as false.
    When set to true a 25 millisecond loop runs boolean checking for changes to global variables assigned to attribute echo and code.
    If it detects a change it will apply the updated Global Variable.
    
    This has been extensible checked for bugs, errors, and code crashes.
    If one is found please report it for fixing.
    
    Bracket notation has not yet been added for recognition but is on the todo list.

--- echo.js
+++ echo.js
@@ -1,83 +1,114 @@
 if (!!window.customElements){
 	(()=>{customElements.define(`echo-`,class extends HTMLElement{
-		static get observedAttributes(){return[`echo`,`code`,`test`]};
+		static get observedAttributes(){return[`echo`,`code`,`auto`]};
 		attributeChangedCallback(name,oldValue){
-			let CR=`color:red`,
-				CB=`color:blue`,
-				CO=`color:orange`,
-				VALUE,
-				INC,
-				CHK,
-				ID =x=>(document.getElementById(x)),
-				GET =x=>(this.getAttribute(x)),
-				ARR =x=>(Array.isArray(x)),
-				TRIM =x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v|[,]/g,` `)),
-				WIN =x=>(window[TRIM(x)]),
-				EVAL =x=>{
+			let E = {
+				CR:`color:red`,
+				CB:`color:blue`,
+				CO:`color:orange`,
+				NULL:x=>(x !== null && x !== undefined),
+				NOT:x=>(name !== x),
+				ID:x=>(document.getElementById(x)),
+				GET:x=>(this.getAttribute(x)),
+				SET:(x,y)=>(this.setAttribute(x,y)),
+				ARR:x=>(Array.isArray(x)),
+				TRIM:x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v|[,]/g,` `)),
+				SPLIT:x=>(E.TRIM(x).split(` `)),
+				WIN:x=>{
+					try {return E.TRIM(x).split('.').reduce((y,z)=>y[z],window)}
+					catch(e){return false}
+				},
+				EVAL:x=>{
 					try {if (!!eval(x)){return true}}
 					catch(e){return false}
 				},
-				CODE = EVAL(GET(`code`)) ? TRIM(eval(GET(`code`)).innerHTML) : GET(`code`),
-				ECHO =x=>{
-					if (!!WIN(x)){
-						CHK = WIN(x);
-						if (CHK !== null && CHK !== undefined){
-							if (typeof CHK == 'string'){x = TRIM(CHK).split(` `)}
-							else if (ARR(CHK)){x = CHK}
-							else if (typeof CHK == 'object' && !(CHK instanceof Element)){
-								VALUE = [];
-								for (INC in CHK){
-									VALUE.push(``+INC);
-								}
-								x = VALUE;
-							}
-						}
+				CODE:x=>{
+					E.CHK = false;
+					if (!!E.WIN(x)){
+						x = E.WIN(x);
+						E.CHK = true;
 					}
-					else if (EVAL(x) && ARR(eval(x))){x = eval(x)}
-					if (!ARR(x)){x = TRIM(x).split(` `)}
+					else if (E.EVAL(x)){x = E.TRIM(eval(x).innerHTML)}
+					else {x = E.GET(`code`)}
+					x = ``+x;
+					if (!x.startsWith(`<`) || !x.endsWith(`>`)){
+						console.log(this.outerHTML);
+						x ? console.log(`%cERROR: ATTRIBUTE "code" %c${x} %cis not a proper HTML code string.  %cDefaulting to: %c<div></div>%c.`,E.CR,E.CB,E.CR,E.CO,E.CB,E.CO)
+							 : console.log(`%cERROR: TAG <echo-> is missing ATTRIBUTE "code". %cDefaulting to: %c<div></div>%c.`,E.CR,E.CO,E.CB,E.CO);
+						x =`<div></div>`;
+						E.CHK = false;
+					}
+					if (E.CHK){E.LOOP(`code`,x)}
 					return x;
 				},
-				OBJ = {};
-			if (GET(`echo`) != '[object Object]'){
-				if (!CODE || !CODE.startsWith(`<`) || !CODE.endsWith(`>`)){
-					console.log(this.outerHTML);
-					CODE ? console.log(`%cERROR: ATTRIBUTE "code" %c${CODE} %cis not a proper HTML code string.  %cDefaulting to: %c<div></div>%c.`,CR,CB,CR,CO,CB,CO)
-						 : console.log(`%cERROR: TAG <echo-> is missing ATTRIBUTE "code". %cDefaulting to: %c<div></div>%c.`,CR,CO,CB,CO);
-					CODE =`<div></div>`;
-				}
+				ECHO:(x,y=false)=>{
+					E.CHK = false;
+					if (!!E.WIN(x)){E.CHK = E.WIN(x);}
+					if (typeof E.CHK == `string`){
+						x = E.SPLIT(E.CHK);
+						y = true;
+					}
+					else if (E.ARR(E.CHK)){
+						x = E.CHK;
+						y = true;
+					}
+					else if (typeof E.CHK === `object` && E.NULL(E.CHK) && !(E.CHK instanceof Element)){
+						E.VALUE = [];
+						for (E.INC in E.CHK){E.VALUE.push(``+E.INC)}
+						x = E.VALUE;
+						y = true;
+					}
+					else if (E.EVAL(x) && E.ARR(eval(x))){x = eval(x)}
+					if (!E.ARR(x)){x = E.SPLIT(x)}
+					if (y == true){E.LOOP(`echo`,E.CHK)}
+					return x;
+				},
+				OBJ:{},
+				LOOP:(x,y)=>{
+					if (E.GET(`auto`) === `true` && E.NULL(x) && E.WIN(E.GET(x)) !== y){
+						y = E.WIN(E.GET(x));
+						E.SET(x ,E.GET(x));
+					}
+					this[x] = setTimeout(E.LOOP,25,x,y);
+				},
+			}
+			if (E.GET(`echo`) !== `[object Object]`){
 				if (name == `echo` && oldValue){
-					VALUE = ECHO(oldValue);
-					for(INC = 0; INC < VALUE.length; INC++){
-						if(ID(VALUE[INC])){
-							OBJ[VALUE[INC]] = ID(VALUE[INC]).outerHTML
-						}
+					E.VALUE = E.ECHO(oldValue);
+					for(E.INC = 0; E.INC < E.VALUE.length; E.INC++){
+						E.CHK = E.VALUE[E.INC];
+						if(E.ID(E.CHK)){E.OBJ[E.CHK] = E.ID(E.CHK).outerHTML}
 					}
 				}
 				if (this.hasAttribute(`echo`)){
-					VALUE = ECHO(GET(`echo`));
+					E.VALUE = E.ECHO(E.GET(`echo`));
 					this.innerHTML = ``;
-					if(VALUE[0] !== ``){
-						for(INC = 0; INC < VALUE.length; INC++){
-							if (typeof VALUE[INC] !== 'object' && typeof VALUE[INC] !== undefined){
-								this.insertAdjacentHTML( `beforeend` , (OBJ[VALUE[INC]] ? OBJ[VALUE[INC]] : CODE));
-								this.lastElementChild.id = ``+VALUE[INC];
-							} else {console.log(`%cERROR%c: Invalid ID Data Type.`,CR,CO)}
+					if(E.VALUE[0] !== ``){
+						E.CODE = E.CODE(E.GET(`code`));
+						for(E.INC = 0; E.INC < E.VALUE.length; E.INC++){
+							E.CHK = E.VALUE[E.INC];
+							if (typeof E.CHK !== `object` && E.NULL(E.CHK)){
+								this.insertAdjacentHTML( `beforeend` , (E.OBJ[E.CHK] ? E.OBJ[E.CHK] : E.CODE));
+								this.lastElementChild.id = ``+E.CHK;
+							} else {console.log(`%cERROR%c: Invalid ID Data Type.`,E.CR,E.CO)}
 						}
 					}
 				}
 				window.addEventListener(`load`,()=>{
-					if (!!WIN(GET(`echo`))){
-						CHK = WIN(GET(`echo`));
-						if (CHK !== null && CHK !== undefined){
-							if (typeof CHK == 'string' || ARR(CHK) || (typeof CHK == 'object' && !(CHK instanceof Element))){
-								this.setAttribute(`echo` , GET(`echo`));
+					if (!!E.WIN(E.GET(name)) && E.NOT(`auto`)){
+						E.CHK = E.WIN(E.GET(name));
+						if (E.NULL(E.CHK)){
+							if (typeof E.CHK === `string` || (E.NOT(`code`) && E.ARR(E.CHK)) || (E.NOT(`code`) && typeof E.CHK === `object` && !(E.CHK instanceof Element))){
+								E.SET(name ,E.GET(name));
+								E.LOOP(name,E.CHK);
 							}
 						}
 					}
-				})
+				});
 			} else {
-				console.log(`%cERROR%c: An object literal was passed as a string, resulting in echo="[object Object]".  HTML cannot process object literals.`,CR,CO);
+				console.log(`%cERROR%c: An object literal was passed as a string, resulting in echo="[object Object]".  HTML cannot process object literals.`,E.CR,E.CO);
 			}
 		}
 	})})()
+} else {console.log(`%cERROR%c: WEB COMPONENTS are Disabled`,E.CR,E.CO);}
 //Written by Tygari Katarana Davis
