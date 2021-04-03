49d0cfe369124d16d2c8c7ae5bf7f76e8530d962
Tygari <26011255+tygari@users.noreply.github.com>
2021-04-03 16:58:09 -0400

    Update echo.js

--- echo.js
+++ echo.js
@@ -1,15 +1,16 @@
 if (!!window.MutationObserver){//Checks if Mutation Observer is supported by Browser
 	(new MutationObserver(M=>{//Sets an Observer on the HTML tag to watch for all new tags added and initialize an Observer on all tags with the ECHO attribute.
-		let O = {
+		let O = { // Variable is initialized within a function to prevent Global Collision, all further variables are keys of this object.
 			CR:`color:red`,
 			CB:`color:blue`,
 			CO:`color:orange`,
-			NULL:x=>(x !== null && x !== undefined),
-			GET:(E,x)=>(E.getAttribute(x)),
-			SET:(E,x,y)=>(E.setAttribute(x,y)),
-			ARR:x=>(Array.isArray(x)),
+			NULL:x=>(x !== null && x !== undefined), // Checks if argument is null or undefined
+			GET:(E,x)=>(E.getAttribute(x)), // Gets an attribute
+			SET:(E,x,y)=>(E.setAttribute(x,y)), // Sets an attribute
+			ARR:x=>(Array.isArray(x)), // Checks if argument is an array
+			NUM:x=>(typeof x !== `boolean` && Number.isFinite(Number(x))), // Checks if argument is a legal number
 			TRIM:x=>(x.trim().replace(/\s\s+|\r|\n|\t|\f|\v|[,]/g,` `)),//Replaces all white space with a single space
-			SPLIT:x=>(O.TRIM(x).split(` `)),
+			SPLIT:x=>(O.TRIM(x).split(` `)), // Splits string into an array and removes all excess white space
 			WIN:x=>{//Searches the Window Object for a passed pathway
 				try {
 					return x.replace(/['"`]/g,'').split('.').reduce(
@@ -41,14 +42,10 @@ if (!!window.MutationObserver){//Checks if Mutation Observer is supported by Bro
 				if (O.CHK){O.AUTO(E,`code`,x)}//Initiates the Auto function for watched varaibles
 				return x;
 			},
-			ECHO:(E,x,y=false)=>{
-				O.CHK = false;
+			ECHO:(E,x)=>{
+				O.CHK = y = false;
 				if (x !== `` && !!O.WIN(x)){O.CHK = O.WIN(x);}//Searches the Window Object for a varaible stored Echo resource.
-				if (typeof O.CHK == `string`){
-					x = O.SPLIT(O.CHK);
-					y = true;
-				}
-				else if (O.ARR(O.CHK)){//Checks if the ECHO attribute is passed an array by a varaible
+				if (typeof O.CHK == `string` || O.NUM(O.CHK) || O.ARR(O.CHK)){//Checks if the ECHO attribute is passed an array by a varaible
 					x = O.CHK;
 					y = true;
 				}
@@ -58,11 +55,12 @@ if (!!window.MutationObserver){//Checks if Mutation Observer is supported by Bro
 					x = O.VALUE;
 					y = true;
 				}
-				else if (x.startsWith(`[`) && x.endsWith(`]`)){//Checks if the ECHO attribute is SET with an array. and converts it
+				else if (x.startsWith(`[`) && x.endsWith(`]`)){//Checks if the ECHO attribute is SET with an array and converts it
 					x = x.replace(/[,]/g,' ').replace(/['"\[\]]/g,'');
 					x = O.SPLIT(x);
 				}
-				if (!O.ARR(x)){x = O.SPLIT(x)}//Converts a word string list to an array
+				if (O.NUM(x)){x = Number(x)}
+				if (typeof x == `string` && !O.ARR(x)){x = O.SPLIT(x)}//Converts a word string list to an array
 				if (y == true){O.AUTO(E,`echo`,O.CHK)}//Initiates the Auto function for watched varaibles
 				return x;//Returns the final array
 			},
@@ -76,36 +74,56 @@ if (!!window.MutationObserver){//Checks if Mutation Observer is supported by Bro
 			WATCH:E=>{
 				(new MutationObserver(R=>{
 					R = R[0];
-					O.OBJ = {};
-					E.echoAuthor = `Tygari Katarana Davis`; //Author's Signature, Please Do Not Remove
+					E.echoAuthor = `Tygari Katarana Davis`; // Author's Signature, Please Do Not Remove
 					E.echoWatch = true; // Boolean to prevent multiple Observers on an element
-					E.echoSet =x=>(O.SET(E,`echo`,x));//Retrieves the ECHO attribute string
+					E.echoSet =x=>(O.SET(E,`echo`,x)); // Retrieves the ECHO attribute string
 					E.echo = O.GET(E,`echo`); // HTML Echo Attribute Retrival
-					if (E.echo !== `[object Object]`){  //Check that HTML Echo Attribtue was not passed an Object directly
-						if (R.attributeName === `echo` && E.oldArray){//Check that both requirements to store prechange values are present
-							E.oldArray.forEach(v=>{if (!!E.children[v]){O.OBJ[v] = E.children[v].outerHTML}});//Store OldValue Data within an Object
+					if (E.echo !== `[object Object]`){ // Check that HTML Echo Attribtue was not passed an Object directly
+						if (R.attributeName === `echo` && !!E.oldArray){
+							if (typeof E.oldArray !== `number`){ // Check that both requirements to store prechange values are present
+								O.OBJ = {};
+								E.oldArray.forEach(v=>{if (!!E.children[v]){O.OBJ[v] = E.children[v].outerHTML}}); // Store OldValue Data within an Object
+							}
+							else if (typeof E.oldArray === `number`){
+								O.OBJ = [];
+								for(O.INC = 0; O.INC < E.children.length; O.INC++){
+									O.OBJ[O.INC] = E.children[O.INC].outerHTML
+								}
+							}
 						}
 						if (E.hasAttribute(`echo`)){
-							O.CHK = O.ECHO(E,E.echo);//Converts the ECHO attribute string to an array
-							O.CHK.forEach((v,i,a)=>{a[i] = O.TRIM(v).replace(/ /g,``)});//removes all white space within the array
-							O.CHK = O.CHK.filter(x=>{return x.length > 0});// filters out all zero lenth array values
-							if (JSON.stringify(O.CHK) !== JSON.stringify(E.echoArray)){//Checks for changes from the old Array and the new Array.
-								E.oldArray = E.echoArray = O.CHK;//Stores the final Array within the Element Object and a backup
-								E.code = O.GET(E,`code`);//Retrieves the CODE attribute string
-								E.codeHTML = O.CODE(E,E.code);//Store a the finalized HTML CODE attribute string within the Element Object
-								E.innerHTML = ``;//Deletes all Children
-								for(O.INC = 0; O.INC < E.echoArray.length; O.INC++){//Adds all Children based on NewValue and OldValue
-									O.CHK = E.echoArray[O.INC];
-									if (typeof O.CHK !== `object` && O.NULL(O.CHK)){
-										O.CHK = ``+O.CHK
-										if (!!document.getElementById(O.CHK)){
-											console.log(`%cERROR%c: ID ${O.CHK} has multiple instances.`,O.CR,O.CO)
-										}
-										E.insertAdjacentHTML( `beforeend` , (O.OBJ[O.CHK] ? O.OBJ[O.CHK] : E.codeHTML));
-										E.lastElementChild.id = O.CHK;
-									} else {console.log(`%cERROR%c: Invalid ID Data Type.`,O.CR,O.CO)}
+							O.CHK = O.ECHO(E,E.echo); // Converts the ECHO attribute string to an array
+							if (O.ARR(O.CHK)){
+								O.CHK.forEach((v,i,a)=>{a[i] = O.TRIM(v).replace(/ /g,``)}) // Removes all white space within array values
+								O.CHK = O.CHK.filter(x=>{return x.length > 0}); // Filters out all zero lenth array values
+								}; // Removes all white space within the array
+							
+							if (JSON.stringify(O.CHK) !== JSON.stringify(E.echoArray)){ // Checks for changes from the old Array and the new Array.
+								E.oldArray = E.echoArray = O.CHK; // Stores the final Array within the Element Object and a backup
+								E.code = O.GET(E,`code`); // Retrieves the CODE attribute string
+								E.codeHTML = O.CODE(E,E.code); // Store a the finalized HTML CODE attribute string within the Element Object
+								E.innerHTML = ``; // Deletes all Children
+								if (O.ARR(E.echoArray)){
+									if (!O.OBJ){O.OBJ = {}}
+									for(O.INC = 0; O.INC < E.echoArray.length; O.INC++){ // Adds all Children based on NewValue and OldValue
+										O.CHK = E.echoArray[O.INC];
+										if (typeof O.CHK !== `object` && O.NULL(O.CHK)){
+											O.CHK = ``+O.CHK
+											if (!!document.getElementById(O.CHK)){
+												console.log(`%cERROR%c: ID ${O.CHK} has multiple instances.`,O.CR,O.CO)
+											}
+											E.insertAdjacentHTML( `beforeend` , (O.OBJ[O.CHK] ? O.OBJ[O.CHK] : E.codeHTML));
+											E.lastElementChild.id = O.CHK;
+										} else {console.log(`%cERROR%c: Invalid ID Data Type.`,O.CR,O.CO)}
+									}
 								}
-								for (O.INC of R.target.querySelectorAll(`[echo]`)){//Checks all children for ECHO Attribute and initiates Observer
+								if (O.NUM(E.echoArray)){
+									if (!O.OBJ){O.OBJ = []}
+									for(O.INC = 0; O.INC < E.echoArray; O.INC++){
+										E.insertAdjacentHTML( `beforeend` , (O.OBJ[O.INC] ? O.OBJ[O.INC] : E.codeHTML));
+									}
+								}
+								for (O.INC of R.target.querySelectorAll(`[echo]`)){ // Checks all children for ECHO Attribute and initiates Observer
 									if (O.INC.echoWatch !== true){
 										O.WATCH(O.INC);
 										O.SET(O.INC,`echo`,O.GET(O.INC,`echo`));
@@ -124,14 +142,14 @@ if (!!window.MutationObserver){//Checks if Mutation Observer is supported by Bro
 				}));
 			},
 		}
-		for (O.INC of M){//Check of the Document for Initializing ECHO Observers
+		for (O.INC of M){ // Check of the Document for Initializing ECHO Observers
 			O.INC = O.INC.type === `childList` ? O.INC.addedNodes[0] : O.INC.target;
 			if (O.INC && O.INC.attributes && O.INC.hasAttribute(`echo`) && O.INC.echoWatch !== true){
 				O.WATCH(O.INC);
 				O.SET(O.INC,`echo`,O.GET(O.INC,`echo`));
 			}
 		}
-		window.addEventListener(`load`,()=>{//Final check of the loaded Document for Initializing ECHO Observers
+		window.addEventListener(`load`,()=>{ // Final check of the loaded Document for Initializing ECHO Observers
 			for (O.INC of document.querySelectorAll(`[echo]`)){
 				if (O.INC.echoWatch !== true){
 					O.WATCH(O.INC);
@@ -139,7 +157,7 @@ if (!!window.MutationObserver){//Checks if Mutation Observer is supported by Bro
 				O.SET(O.INC,`echo`,O.GET(O.INC,`echo`));
 			}
 		});
-	})).observe(document.getElementsByTagName(`html`)[0],{ //Attaches Oberserver to HTML to watch for new Elements and additions of the Echo Attribute
+	})).observe(document.getElementsByTagName(`html`)[0],{ // Attaches Oberserver to HTML to watch for new Elements and additions of the Echo Attribute
 		childList: true,
 		subtree: true,
 		attributes: true,
